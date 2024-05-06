2e6c38a5a7d982082fe9ae3e4d10d5b7b733c44e
Mubtasim Ihtisham <mubtasim26@gmail.com>
2024-05-07 01:36:22 +0600

    First Push Model Domain

---
+++ Models/Domain/Tag.cs
@@ -0,0 +1,9 @@
+﻿namespace BloggieMVC.Models.Domain
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
