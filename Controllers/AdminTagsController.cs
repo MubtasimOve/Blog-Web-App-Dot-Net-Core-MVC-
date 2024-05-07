5051e0a01fa6b98e02517b873f5b8b5a58d14481
Mubtasim Ihtisham <mubtasim26@gmail.com>
2024-05-07 23:06:37 +0600

    Add Layout

---
+++ Controllers/AdminTagsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloggieMVC.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
